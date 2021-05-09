866c917403d2aa88d3b17ee67f7a869d783f26d8
TechnoFuture2020 <mymailforhobby@gmail.com>
2021-05-10 00:43:36 +0300

    Add .html and .css for 768px, 576px and some responsive including 360px

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,5 +10,6 @@ var headerList = document.querySelector('.header-navbar__list');
 
 headerBtn.addEventListener('click', function(){
   this.classList.toggle('header-navbar__btn--active');
+  console.log('THIS is "this": ', this);
   headerList.classList.toggle('header-navbar__list--active')
 })
